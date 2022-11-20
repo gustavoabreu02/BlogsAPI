05d57b8be61a6c4b7a4d3fdf49068c7e7c38bdd0
Gustavo Abreu <gustavofart371@gmail.com>
2022-11-20 15:13:15 -0300

    req1

---
+++ src/migrations/20221120181004-create-blog_posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
