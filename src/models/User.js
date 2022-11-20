c5d01a5b70068930e4540d8a6c81ecb87024954d
Gustavo Abreu <gustavofart371@gmail.com>
2022-11-20 15:19:09 -0300

    req2

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const e = require("express");
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+  return User;
+}
+
+module.exports = UserModel;
